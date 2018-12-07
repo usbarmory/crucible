cf9c101ae3e06e8787f63dcb0c0cdc5bd7fccc02
Andrea Barisani <andrea@inversepath.com>
2018-12-07 18:24:53 +0100

    nvmem-imx-ocotp allows only one complete OTP word write at a time

--- lib/blow.go
+++ lib/blow.go
@@ -80,14 +80,24 @@ func (fusemap *FuseMap) Blow(devicePath string, name string, val []byte) (res []
 		return
 	}
 
-	_, err = device.Seek(int64(addr), 0)
+	wordSize, _, err := driverParams(fusemap.Driver)
 
 	if err != nil {
-		_ = device.Close()
 		return
 	}
 
-	_, err = device.Write(res)
+	// nvmem-imx-ocotp allows only one complete OTP word write at a time
+	for i := 0; i < len(res); i += int(wordSize) {
+		_, err = device.Seek(int64(addr) + int64(i), 0)
+
+		if err != nil {
+			_ = device.Close()
+			return
+		}
+
+		_, err = device.Write(res[i:i+int(wordSize)])
+	}
+
 	_ = device.Close()
 
 	return
