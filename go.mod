87646a31cf094eb5f954d7b5b01dbc188642173f
Andrea Barisani <andrea@inversepath.com>
2019-01-21 09:49:59 +0100

    migrate to go module

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/inversepath/crucible
+
+require (
+	github.com/ghodss/yaml v1.0.0
+	github.com/kr/pretty v0.1.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
---
+++ internal/crucible.go
@@ -0,0 +1,13 @@
+// crucible
+// One-Time-Programmable (OTP) fusing tool
+//
+// Copyright (c) F-Secure Corporation
+//
+// Use of this source code is governed by the license
+// that can be found in the LICENSE file.
+
+package crucible
+
+// build information, initialized at compile time (see Makefile)
+var Revision string
+var Build string
