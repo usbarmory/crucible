c8eccfbd2cb03e6238518664f8f83e138269cd02
Andrea Barisani <andrea@inversepath.com>
2019-04-05 20:48:21 +0200

    simplify implementation by using a build option to force Linux environment

--- internal/driver.go
+++ internal/driver.go
@@ -5,56 +5,16 @@
 //
 // Use of this source code is governed by the license
 // that can be found in the LICENSE file.
+//
+// +build linux
 
 package crucible
 
 import (
 	"errors"
-	"fmt"
-	"syscall"
 )
 
-func getSystemInformation() (kernel string, release string, err error) {
-	var uname syscall.Utsname
-
-	err = syscall.Uname(&uname)
-
-	if err != nil {
-		return
-	}
-
-	for _, c := range uname.Sysname {
-		if c == 0 {
-			break
-		}
-
-		kernel += string(byte(c))
-	}
-
-	for _, c := range uname.Release {
-		if c == 0 {
-			break
-		}
-
-		release += string(byte(c))
-	}
-
-	return
-}
-
 func driverParams(name string) (wordSize uint32, bankSize uint32, err error) {
-	kernel, _, err := getSystemInformation()
-
-	if err != nil {
-		err = fmt.Errorf("could not get system information, %v", err)
-		return
-	}
-
-	if kernel != "Linux" {
-		err = errors.New("unsupported OS")
-		return
-	}
-
 	switch name {
 	case "nvmem-imx-iim":
 		wordSize = 1
